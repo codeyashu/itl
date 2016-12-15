ad5b018605ee0d8abfab817e4bbd64215574f3ad
codeyashu <yashasycsc@outlook.com>
2016-12-16 02:17:16 +0530

    sort array of objects

--- server.js
+++ server.js
@@ -37,36 +37,72 @@ app.use('/',router)
 app.use(express.static(__dirname + '/public'))
 
 
-//get nearest traffic Signal
+//sort array of objects
+var sort_by = function(field, reverse, primer){
+   var key = function (x) {return primer ? primer(x[field]) : x[field]};
+
+   return function (a,b) {
+	  var A = key(a), B = key(b);
+	  return ( (A < B) ? -1 : ((A > B) ? 1 : 0) ) * [-1,1][+!!reverse];                  
+   }
+}
+
+//get distance to traffic Signal
 function getnearest(loclat,loclong){
-     var min = 10000;
+    // var min = 10000;
      var lat = loclat;
      var long = loclong;
-     var ssid;
-     var ssplace;
+
+     var testnear = new Object();
            
      for(let i = 0; i < query.slen; i++){
         (function(){
-            var distance = formula.distance(lat,long,query.slist[i].location.lat,query.slist[i].location.long);
-            if(distance < min){
+            var sdist = formula.distance(lat,long,query.slist[i].location.lat,query.slist[i].location.long);
+
+            testnear[i].sdist = sdist;
+            testnear[i].sid = query.slist[i].id;
+            testnear[i].splace = query.slist[i].place;
+          
+          /*  if(distance < min){
                min = distance;
                ssid = query.slist[i].id;
                ssplace = query.slist[i].place;
             }
+        */ 
         }());
      }
+
+     testnear.sort(sort_by('sdist', true, parseFloat));
+
+/*
      return {
          distance : min,
          sid : ssid,
          splace : ssplace
      }
+
+     */
+
+    return testnear;
 }
 
+
+function checkside(loclat,loclong,ssid){
+    var sidearray = new Array;
+    var minside;
+    sidearray[0] =  formula.distance(lat,long,query.slist[ssid].cpoint.lat1,query.slist[ssid].cpoint.long1);
+    sidearray[1] =  formula.distance(lat,long,query.slist[ssid].cpoint.lat2,query.slist[ssid].cpoint.long2);
+    sidearray[2] =  formula.distance(lat,long,query.slist[ssid].cpoint.lat3,query.slist[ssid].cpoint.long3);
+    sidearray[3] =  formula.distance(lat,long,query.slist[ssid].cpoint.lat4,query.slist[ssid].cpoint.long4);
+    return arr.indexOf(Math.max(...sidearray));
+   // return Math.min(...sidearray);
+}
 
 
 ///--socket.io--///
 
-var momu = 0
+var momu = -1;
+var domu = -1;
 var locarray = new Array()
 
 io.on('connection',function(socket){
@@ -94,47 +130,62 @@ io.on('connection',function(socket){
         })
     })
 
+
    //On Ambulance Join
     socket.on('createAmbulance',function(chosenAmbulance){
         socket.join('emergency ambulance')
         console.log('Ambulance '+chosenAmbulance+' in emergency')
         console.log('Waiting For Location Data')
     })
-
-
-
     
    
    //On Location Sent By App
     socket.on('location sent',function(loclat,loclong){
          
-         
-         
          console.log("latitude: "+ loclat)
          console.log("longitude: "+ loclong)
 
          console.log("Calculating distances to traffic signal")
-         var nearest = getnearest(loclat,loclong)
+
+         var testnear = getnearest(loclat,loclong)
+
+         console.log("Traffic Signal Order is ")
+         console.dir(testnear)
          momu++;
-         console.log("Nearest signal " +nearest.sid)
-         console.log("Location " +nearest.splace)
-         console.log("Distance from Ambulance " +nearest.distance + " Km")
+         domu++;
+         console.log("Nearest signal " +testnear[domu].sid)
+         console.log("Place " +testnear[domu].splace)
+         console.log("Distance from Ambulance " +testnear[domu].sdist + " Km")
          
-         console.log("Checking if Approaching "+nearest.splace + " Updating Array");
+         console.log("Checking if Approaching "+testnear[domu].splace + " ---Updating Array");
 
          (function(){
-             locarray[momu] = nearest.distance
+             locarray[momu] = testnear[domu].sdist
              if(momu === 10){
-                 if((locarray[1]-locarray[10]) > 0){
-                     console.log("Confirmed Approaching")
+                 if((locarray[0]-locarray[10]) > 0){
+                     console.log("\n.\n.Confirmed Approaching\n.\n.")
+                     console.log("Checking Side")
+                     var sside = checkside(loclat,loclong,testnear[domu].sid)
+                     sside++;
+                     console.log("testnear[domu] Side is "+ sside)
+                     showgreen(sside);
+                 }
+                 else{
+                     console.log("Moving away from signal "+ testnear[domu].splace)
+                     console.log("Checking Next Signal")
+                     domu++;
                  }
              }
 
-         }());
-           
-         io.emit('emergency',"1001");
+         }());  
             
       })
+
+
+      // emit green signal
+   function showgreen(sside){
+       io.emit('emergency',"1001");
+   }
       
  })
 
@@ -143,11 +194,7 @@ io.on('connection',function(socket){
 
 function gaat(){
   var x = getnearest(12.917049,77.636326)
-
-  console.log(x.distance)
- // console.log(query.slen)
- // console.log(query.slist[0].location.lat)
-  //console.log(query.slist)
+  console.dir(x);
 }
 setTimeout(gaat,3000);
       
